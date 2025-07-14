ce9ee740919ed5e4352407881fa14792a9640896
VPangan <vince_pangan@dlsu.edu.ph>
2025-07-14 11:46:14 +0800

    Update DBConnection.java

--- CCINFOM S12-GROUP 8-DBAPP-FULL/src/model/DBConnection.java
+++ CCINFOM S12-GROUP 8-DBAPP-FULL/src/model/DBConnection.java
@@ -4,12 +4,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBConnection {
+public class DBConnector {
     private static final String URL = "jdbc:mysql://localhost:3306/barangay_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = ""; // Put your MySQL password inside "".
 
     public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // ⬅️ Explicitly load the MySQL driver
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL JDBC Driver not found", e);
+        }
+
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
+
 }
